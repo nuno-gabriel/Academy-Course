37405710e008a597575ff1e721ee3c48769b3f50
nuno-gabriel <98390802+nuno-gabriel@users.noreply.github.com>
2022-05-02 13:20:52 +0100

    added exercise 35

---
+++ src/Exercise3/exercise35.java
@@ -0,0 +1,37 @@
+package Exercise3;
+
+public class exercise35 {
+/*
+
+      a) int n = 0;
+while( n < 10 ) {
+        System.out.print( n );
+    }
+    Answer: It misses the incremental n++
+      */
+
+     /* b) int b = 0;
+
+while( n < 10 );{
+
+        System.out.print( n );
+
+        n++;
+        Answer: It misses a break
+      */
+        /*
+        c)
+        int c = 0, y = 0, x = 1;
+
+        while( n = 0 ){
+
+            y = y + x;
+
+            x++;
+
+            if(x > 5) n = 1;
+
+        }
+}
+        Answer: It should be n == 0 in the while loop to the boolean to be
+         */
